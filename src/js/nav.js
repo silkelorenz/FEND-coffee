a9dea9f9a8143fb3b225859d085fa4f60010c7da
Silke Lorenz <silke-lorenz@web.de>
2023-03-29 20:30:01 +0200

    bug fixed

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,7 @@
-const button = document.querySelector('btn');
+const button = document.querySelector('.btn');
 
 button.addEventListener('click', clickHandler);
 
-function clickHandler()
-alert('clicked')
+function clickHandler() {
+    alert('clicked');
+}
